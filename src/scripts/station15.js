82fb10fe4cd05a29563ae12e68980e12880bc591
05f2b9db-0772-4f22-a042-97e09605bf34 <203386425+05f2b9db-0772-4f22-a042-97e09605bf34@users.noreply.github.com>
2025-03-25 18:55:30 +0900

    test: Pass "Station 15"

---
+++ src/scripts/station15.js
@@ -0,0 +1,9 @@
+function displayList() {
+    document.getElementById("fruits").innerHTML = `
+        <ul>
+            <li>リンゴ</li>
+            <li>イチゴ</li>
+            <li>ブドウ</li>
+        </ul>
+    `;
+}
